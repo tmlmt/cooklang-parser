322b6bb11f09c088cffcc26d30a28dc7f8e90fbf
Thomas Lamant <tom@tmlmt.com>
2026-01-16 02:16:22 +0100

    docs(playground): add page title and description

--- playground/app/app.vue
+++ playground/app/app.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: "Cooklang Parser Playground",
+  ogTitle: "Cooklang Parser Playground",
+  description:
+    "An interactive playground for the Cooklang Parser library @tmlmt/cooklang-parser, allowing you to edit and render Cooklang recipes in real-time.",
+  ogDescription:
+    "An interactive playground for the Cooklang Parser library @tmlmt/cooklang-parser.",
+});
+</script>
+
 <template>
   <UApp :toaster="{ position: 'top-right' }">
     <NuxtPage />
