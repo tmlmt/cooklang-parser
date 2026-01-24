fcd2302d851f7dfd218e3069ed594f69ff32d7e9
Thomas Lamant <tom@tmlmt.com>
2026-01-24 06:11:28 +0100

    chore(vitest): do not run coverage by default

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     coverage: {
       provider: "v8",
       reporter: ["text", "html"],
-      enabled: true,
+      enabled: false,
       include: ["src/**"],
       exclude: ["src/types.ts", "src/index.ts", "src/utils/general.ts"],
     },
